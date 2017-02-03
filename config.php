0ce03cec97acb0c62a2a765fc858f692a27060d3
KaHing <kahing.wong.js@gmail.com>
2017-02-02 16:19:19 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ define('DB_host', 'localhost');
+
+ define('DB_user', 'admin' ); // create a new db_user just for user management is recommended
+ define('DB_password', ''); // use a strong password
+
+ define('DB_name' , 'users');
+

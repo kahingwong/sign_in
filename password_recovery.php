fd022f78a768e63ff592c6f7cffdca507a76f73b
KaHing <kahing.wong.js@gmail.com>
2017-02-02 16:21:58 -0800

    Create password_recovery.php

---
+++ password_recovery.php
@@ -0,0 +1,20 @@
+<?php
+ $title = "Password Recovery";
+
+ $notice =
+ "Please contact your supervisor in person to reset your password.<br>\n".
+ "The original designers & programmers does not know any of your passwords by design.<br>\n".
+ "The system administrator does not know any of your passwords.<br>\n".
+ "\n";
+?>
+
+<HTML>
+<HEAD>
+ <TITLE><?php echo $title; ?></TITLE>
+</HEAD>
+<BODY >
+ <div id=notice >
+ <?php echo $notice; ?>
+ </div>
+</BODY>
+</HTML>
